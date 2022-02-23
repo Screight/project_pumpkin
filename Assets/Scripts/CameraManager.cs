9764f86d2d7f4ff8edddae8b419e66a3b0a384ae
Screight <seguer.matias@gmail.com>
2022-02-23 01:38:10 +0100

    Revert "Merge branch 'main' of https://github.com/Screight/project_pumpkin"
    This reverts commit 70bd47c12c0729b4414720066100c9e73754bb00, reversing
    changes made to 2c07e0dae243ffe37e24d1144620a76de96c0c25.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager m_instance;
+
     [SerializeField] Camera camera;
 
     int screenWidth;
@@ -34,9 +35,14 @@ public class CameraManager : MonoBehaviour
         topLimit = topLeftCorner.y;
         rightLimit = bottomRightCorner.x;
         bottomLimit = bottomRightCorner.y;
+
+
     }
 
-    private void Update() { UpdateCameraPosition(); }
+    private void Update()
+    {
+        UpdateCameraPosition();
+    }
 
     public float LeftLimit { get { return leftLimit; } }
     public float RightLimit { get { return rightLimit; } }
@@ -56,7 +62,14 @@ public class CameraManager : MonoBehaviour
         topLimit = topLeftCorner.y;
         rightLimit = bottomRightCorner.x;
         bottomLimit = bottomRightCorner.y;
+
     }
 
-    static public CameraManager Instance { get { return m_instance; } }
+    static public CameraManager Instance
+    {
+        get { return m_instance; }
+    }
+
+
+
 }
