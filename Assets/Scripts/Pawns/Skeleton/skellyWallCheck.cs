b7e750ecaf9660651d28b35ac4e02131acd38d27
Arnau Enti <arnaum03@gmail.com>
2022-03-16 09:46:37 +0100

    pequeños ajustes de animación de ataque (Isma)

--- Assets/Scripts/Pawns/Skeleton/skellyWallCheck.cs
+++ Assets/Scripts/Pawns/Skeleton/skellyWallCheck.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyWallCheck : MonoBehaviour
+public class skellyWallCheck : MonoBehaviour
 {
     Skeleton skeletonScript;
     Rigidbody2D skeletonRigidBody;
