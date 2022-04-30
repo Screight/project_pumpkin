0aa71002319c67c8a8f32d39199fe24d123eaf2b
Kitsune03 <arnaum03@gmail.com>
2022-04-30 23:41:05 +0200

    Cutscenes 4.1 - 4.5 finished :sparkle

--- Assets/Scripts/Managers/Timeline.cs
+++ Assets/Scripts/Managers/Timeline.cs
@@ -7,9 +7,11 @@ public class Timeline : MonoBehaviour
 {   
     [SerializeField] bool m_isCameraScripted = false;
     [SerializeField] bool m_isFireSpiritCutScene = false;
+    [SerializeField] bool m_isDarknessSpiritCutScene = false;
     [SerializeField] bool m_hideHud = false;
     [SerializeField] bool m_playerCanMove = false;
     public bool m_cutSceneStartsWithDialog = false;
+    [SerializeField] GameObject[] m_GameObjectsToActivate;
 
     public PauseMenu pausemenu;
     public PlayableDirector m_director;
@@ -19,6 +21,7 @@ public class Timeline : MonoBehaviour
 
     private void Start() 
     {
+        for (int i = 0; i < m_GameObjectsToActivate.Length; i++) { m_GameObjectsToActivate[i].SetActive(false); }
         pausemenu = FindObjectOfType<PauseMenu>();
         hasPlayed = false;
         hasStartedPlaying = false; 
@@ -39,6 +42,7 @@ public class Timeline : MonoBehaviour
     public void startCutScene()
     {
         //Debug.Log("Empieza CutScene");
+        for (int i = 0; i < m_GameObjectsToActivate.Length; i++) { m_GameObjectsToActivate[i].SetActive(true); }
         pausemenu.IsCutScenePlaying = true;
         if (m_isCameraScripted) { CameraManager.Instance.SetCameraToStatic(); }
         if (m_hideHud) { m_HUD.enabled = false; }
@@ -56,6 +60,7 @@ public class Timeline : MonoBehaviour
         if (m_isCameraScripted) { CameraManager.Instance.SetCameraToNormal(); }
         if (m_hideHud) { m_HUD.enabled = true; }
         if (m_isFireSpiritCutScene) { GameManager.Instance.SetIsSkillAvailable(SKILLS.FIRE_BALL, true); }
+        if (m_isDarknessSpiritCutScene) { GameManager.Instance.SetIsSkillAvailable(SKILLS.DASH, true); }
         gameObject.SetActive(false);
     }
 
