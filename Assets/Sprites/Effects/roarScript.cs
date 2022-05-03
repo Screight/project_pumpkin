8e9d1526896acf6b3ec7ab57c662080873a360ee
GrimHaagenti <63322264+GrimHaagenti@users.noreply.github.com>
2022-05-03 13:51:53 +0200

    :D

--- Assets/Sprites/Effects/roarScript.cs
+++ Assets/Sprites/Effects/roarScript.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class roarScript: MonoBehaviour
 {
+    public Transform a;
+    public Transform b;
+
     void Update()
     {
-        transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
-        if (transform.localScale.x > 2f)
+        a.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        if (a.localScale.x > 10f)
         {
-            transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            a.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+        } 
+        b.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+        if (b.localScale.x > 5f)
+        {
+            b.localScale = new Vector3(0.01f, 0.01f, 0.01f);
         }
     }
 }
