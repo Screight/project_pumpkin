dbcb5c9913e0fcefb9e85d0f95ae2c5d0e73e77e
Ismael Rivero <ismaelrivero767@gmail.com>
2022-05-08 02:39:02 +0200

    nyam

--- Assets/Scripts/Managers/weakPlatformDestroyer.cs
+++ Assets/Scripts/Managers/weakPlatformDestroyer.cs
@@ -1,30 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class weakPlatformDestroyer : MonoBehaviour
 {
-    private SpriteRenderer platformSprite;
+    private TilemapRenderer platformSprite;
+    private Tilemap platform_render;
+        
     private GameObject player;
     private Player playerScript;
+    [SerializeField] Material shake_mat;
+    [SerializeField] Material def_mat;
     
 
     private void Awake() 
     {
-        platformSprite = GetComponent<SpriteRenderer>();
+        platformSprite = GetComponent<TilemapRenderer>();
     }
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         playerScript = player.GetComponent<Player>();
+
+        platformSprite.material = def_mat;
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "groundCheck" && playerScript.State == PLAYER_STATE.GROUNDBREAKER)
+        if (collision.gameObject.tag == "groundCheck" )  
         {
-            platformSprite.color = Color.Lerp(Color.white, Color.red, 10.0f);
-            Destroy(gameObject, 0.1f);
+            platformSprite.material = shake_mat;
+
+            if (playerScript.State == PLAYER_STATE.GROUNDBREAKER) {
+
+                platform_render.color = Color.Lerp(Color.white, Color.red, 10.0f);
+                Destroy(gameObject, 0.1f); }
         }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+       
+            platformSprite.material = def_mat;
+        
+
+    }
 }
+
--- Assets/Sprites/Effects/shakingGShader.shader
+++ Assets/Sprites/Effects/shakingGShader.shader
@@ -5,6 +5,8 @@ Shader "Unlit/shakingGShader"
         _MainTex ("Texture", 2D) = "white" {}
         _Displace ("Displacement Texture", 2D) = "bump" {}
         _strenght("Intensity", Range(0, 1)) = 0.5
+
+        _ScrollSpeeds("Scroll Speeds", vector) = (-5, 0, 0, 0)
         
     }
     SubShader
@@ -34,22 +36,30 @@ Shader "Unlit/shakingGShader"
             };
 
             sampler2D _MainTex, _Displace;
-            float4 _Displace_ST, _MainTex_ST;
+            float4 _Displace_ST, _MainTex_ST, _ScrollSpeeds;
             float _strenght;
             
-
+            
+            float random(float2 p) {
+                return cos(dot(p, float2(23.14069263277926, 2.665144142690225))) * 12345.6789; 
+            }
 
             v2f vert (appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = v.uv;
+                
+                float r = sin(_Time.y * 15);
+                float z = sin(_Time.y * 5);
+
+                o.uv += vector(_ScrollSpeeds.x * r/100, _ScrollSpeeds.y * z/200, 0,0) ;
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
-                _MainTex_ST.x +=  unity_DeltaTime;
+                
                 half4 d = tex2D(_Displace, _MainTex_ST);
                 float4 p = i.uv + (d * _strenght);
                 fixed4 col = tex2D(_MainTex, p);
