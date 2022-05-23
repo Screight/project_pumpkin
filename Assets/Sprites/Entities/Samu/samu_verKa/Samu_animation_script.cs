daa851d19fd756261f9cb86326c2f0b042b5a59b
Ismael Rivero <ismaelrivero767@gmail.com>
2022-05-23 02:49:57 +0200

    zzz

--- Assets/Sprites/Entities/Samu/samu_verKa/Samu_animation_script.cs
+++ Assets/Sprites/Entities/Samu/samu_verKa/Samu_animation_script.cs
@@ -7,6 +7,9 @@ public class Samu_animation_script : MonoBehaviour
 
     [SerializeField] GameObject innerRing;
     [SerializeField] GameObject outerRing;
+    [SerializeField] GameObject mainCircle;
+    [SerializeField] GameObject subCircle1;
+    [SerializeField] GameObject subCircle2;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,13 @@ public class Samu_animation_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      float sin = Mathf.Cos(Time.time/3)/5 ;
+      float sin = Mathf.Cos(Time.time/2)*1.5f ;
         Debug.Log(sin);
         innerRing.transform.Rotate( new Vector3( 0,0,sin));
         outerRing.transform.Rotate(new Vector3(0, 0, - sin));
+        mainCircle.transform.Rotate(new Vector3(0, 0, 0.5f));
+        subCircle1.transform.Rotate(new Vector3(0, 0, -1f));
+        subCircle2.transform.Rotate(new Vector3(0, 0, -1f));
+
     }
 }
