e664331122120565f7175233feee7d4e1144d23b
GrimHaagenti <63322264+GrimHaagenti@users.noreply.github.com>
2022-05-25 10:42:03 +0200

    nyam

--- Assets/Shaders/AttachGameObjectsToParticles.cs
+++ Assets/Shaders/AttachGameObjectsToParticles.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class AttachGameObjectsToParticles : MonoBehaviour
 {
+    private enum LightColorOptions { ParticleColor, CustomColor, Default };
+
     public GameObject m_Prefab;
 
     private ParticleSystem m_ParticleSystem;
     private List<GameObject> m_Instances = new List<GameObject>();
     private ParticleSystem.Particle[] m_Particles;
 
+
+
+    [SerializeField] LightColorOptions colorOpt;
+    [SerializeField] private Color particleColorAvg;
+
+
     // Start is called before the first frame update
     void Start()
     {
         m_ParticleSystem = GetComponent<ParticleSystem>();
         m_Particles = new ParticleSystem.Particle[m_ParticleSystem.main.maxParticles];
+
+        particleColorAvg = (m_ParticleSystem.main.startColor.colorMin + m_ParticleSystem.main.startColor.colorMax) / 2;
+
+        //m_Prefab.GetComponent<Light2D>().color = m_ParticleSystem.main.startColor;
     }
 
     // Update is called once per frame
