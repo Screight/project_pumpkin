f9d668f5aa7363d63151ac279c004979743e3cb9
Ismael Rivero <ismaelrivero767@gmail.com>
2022-06-03 01:57:13 +0200

    aaaa

--- Assets/Sprites/Entities/Samu/samu_verKa/Samu_animation_script.cs
+++ Assets/Sprites/Entities/Samu/samu_verKa/Samu_animation_script.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Samu_animation_script : MonoBehaviour
 {
     enum Anim_States { STOP,BACK2ORIGIN , IDLE, ATK1, ATK1VAR, ATK2};
+    enum BodyParts { CORE, INNER_RING , OUTER_RING}
+    enum Bodies { MAIN, BODY1}
     Anim_States state = Anim_States.STOP;
     Anim_States next_state = Anim_States.STOP;
     Anim_States prev_state = Anim_States.STOP;
 
 
-    [SerializeField] GameObject innerRing;
-    [SerializeField] GameObject outerRing;
+    //[SerializeField] GameObject innerRing;
+    //[SerializeField] GameObject outerRing;
+    //[SerializeField] GameObject core;
     [SerializeField] GameObject mainCircle;
     [SerializeField] GameObject eye_obj;
 
 
     [SerializeField] GameObject[] Samu_bodies;
+    [SerializeField] GameObject[] MainBodyParts;
+    [SerializeField] GameObject[] Body1Parts;
     
+    
+        
     [SerializeField] GameObject atk1_Fireball_pos_obj;
     [SerializeField] Samu_BigFireball fireball_pref;
     private GameObject[] Atk1_fireball_init_pos;
@@ -28,6 +35,7 @@ public class Samu_animation_script : MonoBehaviour
     public Samu_BigFireball[] Atk1var_fireballs;
 
 
+    private GameObject currentBody;
     private GameObject[] eyes;
     private GameObject[] eyes_init_pos;
     private GameObject player;
@@ -36,15 +44,28 @@ public class Samu_animation_script : MonoBehaviour
 
     Vector3 init_pos;
     float time;
+    private float dash_velocity = 5f;
+    private int max_dash_number = 2;
+    private int dash_number = 1;
+    private int dir = 0;
+
     private bool playerOnSight = false;
     private bool canMove = true;
     private bool changeState = false;
     private bool fireballsSummoned = false;
     private bool fireballsThrown = false;
 
-    private float trackingTimer = 3;
+    private float trackingTimer = 1;
     private float trackingTimer_time = 0;
+    private bool tracking = false;
     private bool canGoOffscreen = false;
+    private bool Atk2Finished = false;
+    private bool enraged = false;
+
+
+    Vector3 cam_bounds_;
+    Vector3 init_wPos ;
+
     Vector3 cameraOffscreenPoint;
     Vector3 point;
 
@@ -56,7 +77,9 @@ public class Samu_animation_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // 
+        currentBody = Samu_bodies[0]; 
+
+
         List<GameObject> list = new List<GameObject>();
 
         int eyes_obj_id = eye_obj.GetInstanceID();
@@ -107,7 +130,7 @@ public class Samu_animation_script : MonoBehaviour
         }
 
         Atk1var_fireball_init_pos = list.ToArray();
-        init_pos = gameObject.transform.localPosition;
+        init_pos = currentBody.transform.localPosition;
         Samu_bodies[1].SetActive(false);
     }
 
@@ -117,9 +140,23 @@ public class Samu_animation_script : MonoBehaviour
         ///ROTATION 
         float sin = Mathf.Cos(Time.time / 2) * 1.5f;
         //Debug.Log(sin);
-        innerRing.transform.Rotate(new Vector3(0, 0, sin));
-        outerRing.transform.Rotate(new Vector3(0, 0, -sin));
-        eye_obj.transform.Rotate(new Vector3(0, 0, -sin));
+        if (currentBody == Samu_bodies[((int)(Bodies.MAIN))] )
+        {
+            MainBodyParts[(int)(BodyParts.INNER_RING)].transform.Rotate(new Vector3(0, 0, sin));
+            MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.Rotate(new Vector3(0, 0, -sin));
+        }
+        
+
+        if (!Samu_bodies[1].activeSelf)
+        {
+            eye_obj.transform.rotation = new Quaternion(MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.x, MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.y, MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.z, MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.w);
+        }
+        else
+        {
+            eye_obj.transform.rotation = new Quaternion(Samu_bodies[1].transform.rotation.x, Samu_bodies[1].transform.rotation.y, Samu_bodies[1].transform.rotation.z, -Samu_bodies[1].transform.rotation.w);
+        }
+
+
         mainCircle.transform.Rotate(rot_speed);
 
 
@@ -132,11 +169,14 @@ public class Samu_animation_script : MonoBehaviour
         {
             Atk1var_fireball_init_pos[i].transform.Rotate(rot_speed*4f);
         }
-
+        int eyes_alive = eyes.Length;
         ///EYETRACKING
         for (int i = 0; i < eyes.Length; i++)
         {
-
+            if (!eyes[i].activeSelf)
+            {
+                eyes_alive--;
+            }
             if (playerOnSight)
             {
                 Vector3 offset = player.transform.position - eyes_init_pos[i].transform.position;
@@ -146,13 +186,37 @@ public class Samu_animation_script : MonoBehaviour
                 eyes[i].transform.localPosition = (new Vector3(eyes_init_pos[i].transform.position.x, eyes_init_pos[i].transform.position.y, 0) + new Vector3(eyes_init_pos[i].transform.position.x - direction.x, eyes_init_pos[i].transform.position.y - direction.y, 0) * -1);
             }
             else { eyes[i].transform.position = eyes_init_pos[i].transform.position; }
-            eyes_init_pos[i].transform.Rotate(new Vector3(0, 0, sin));
-
+            if (!Samu_bodies[1].activeSelf)
+            {
+                eyes_init_pos[i].transform.localRotation = new Quaternion(MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.x, MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.y, MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.z, -MainBodyParts[(int)(BodyParts.OUTER_RING)].transform.rotation.w);
+            
+        }
+            else
+            {
+                eyes_init_pos[i].transform.localRotation = new Quaternion(Samu_bodies[1].transform.rotation.x, Samu_bodies[1].transform.rotation.y, Samu_bodies[1].transform.rotation.z, -Samu_bodies[1].transform.rotation.w);
+            }
 
         }
+        if (eyes_alive == 0) { enraged = true; }
+        else { enraged =false; }
+
+        cam_bounds_ = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        init_wPos = transform.TransformPoint(init_pos);
+        //INPUT PROVISIONAL
         if (Input.GetKeyDown(KeyCode.E))
         {
             ATK2();
+
+        } 
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ATK1();
+
+        } 
+        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ATK1VAR();
 
         } 
 
@@ -190,21 +254,21 @@ public class Samu_animation_script : MonoBehaviour
                     float x_pos = Mathf.Cos(time) * 200;
                     float y_pos = Mathf.Sin(time * 2f) * 60;
                 int max_range = 2;
-                Debug.Log(x_pos);
-                Debug.Log(y_pos);
+                //Debug.Log(x_pos);
+               // Debug.Log(y_pos);
 
                 if (changeState)
                 {
-                    if (!(NumberInRange(x_pos, transform.localPosition.x, max_range) && NumberInRange(y_pos, transform.localPosition.y, max_range)))
+                    if (!(NumberInRange(x_pos, currentBody.transform.localPosition.x, max_range) && NumberInRange(y_pos, currentBody.transform.localPosition.y, max_range)))
                     {
-                        Vector3 offset = transform.localPosition - (init_pos + new Vector3(x_pos , -y_pos , 0));
+                        Vector3 offset = currentBody.transform.localPosition - (init_pos + new Vector3(x_pos , -y_pos , 0));
                         Vector3 direction = Vector3.ClampMagnitude(offset, 2.5f);
-                        transform.localPosition += -direction;
+                        currentBody.transform.localPosition += -direction;
                         canMove = false; }
 
                     else{
                         softening_movement_mod = 0.3f;
-                        changeState = false;
+                            changeState = false;
                             canMove = true;
                         }
                 }
@@ -214,12 +278,12 @@ public class Samu_animation_script : MonoBehaviour
 
                     Vector3 motion = init_pos + new Vector3(x_pos* softening_movement_mod, -y_pos* softening_movement_mod, 0);
 
-                    transform.localPosition = motion; }
+                    currentBody.transform.localPosition = motion; }
                   
                     break;
 
                 case Anim_States.BACK2ORIGIN:
-                if (transform.localPosition != init_pos)
+                if (currentBody.transform.localPosition != init_pos)
                 {
                     Back2Origin_f();
                 }
@@ -234,9 +298,22 @@ public class Samu_animation_script : MonoBehaviour
                 
                 break;
             case Anim_States.ATK2:
-                GoOffscreen();
-
-                break;
+                if (!Atk2Finished)
+                {
+                    GoOffscreen();
+                }
+                else
+                {
+                    if (currentBody != Samu_bodies[(int)(Bodies.MAIN)]) { TransferBody(Samu_bodies[(int)(Bodies.MAIN)]); }
+                    Back2Origin_f();
+                }
+                if (currentBody.transform.localPosition == init_pos && Atk2Finished)
+                {
+                    Atk2Finished = false;
+                    canGoOffscreen = false;
+                    state = prev_state; 
+                }
+                    break;
 
             }
        
@@ -246,7 +323,8 @@ public class Samu_animation_script : MonoBehaviour
         {
             UnsummonCircles();
         }
-
+        mainCircle.transform.position = currentBody.transform.position;
+        eye_obj.transform.position = currentBody.transform.position;
         Debug.Log(state);
     }
     private void ThrowFireballs(Samu_BigFireball[] FBs)
@@ -276,63 +354,195 @@ public class Samu_animation_script : MonoBehaviour
 
     private void Back2Origin_f() {
 
-        MoveToPoint(transform, init_pos, 2.5f);
+        MoveToPoint(currentBody.transform, init_pos, 2.5f);
         
         changeState = false;
-
-
+       
     }
     private void GoOffscreen() {
 
-        if (transform.localPosition == init_pos)
+        if (currentBody.transform.localPosition == init_pos)
         {
             canGoOffscreen = true;
         }
 
-        if (transform.localPosition != init_pos && !canGoOffscreen )
+        if (currentBody.transform.localPosition != init_pos && !canGoOffscreen )
         {
             Back2Origin_f();
         }
 
-        if (canGoOffscreen)
+        if (canGoOffscreen && Samu_bodies[(int)(Bodies.MAIN)].activeSelf)
         {
              
             
-            MoveToPoint(transform, point, 2.5f, 1);
+            MoveToPoint(currentBody.transform, point, 2.5f, 1);
 
         }
 
-        if (transform.position.y >= cameraOffscreenPoint.y-init_pos.y && !Samu_bodies[1].activeSelf)
+        if (currentBody.transform.position.y >= cameraOffscreenPoint.y-init_pos.y && !Samu_bodies[1].activeSelf)
         {
-            TransferBody();
+            TransferBody(Samu_bodies[1]);
         }
         
-        if (Samu_bodies[1].activeSelf)
+        if (Samu_bodies[(int)(Bodies.BODY1)].activeSelf)
         {
-            Vector3 cam_bounds_ = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-            Vector3 init_wPos = transform.TransformPoint(init_pos);
-
-            MoveToPoint(Samu_bodies[1].transform, new Vector3(cam_bounds_.x+init_pos.x/2-15, player.transform.position.y, init_wPos.z - init_pos.z), 1f,1);
-            mainCircle.transform.position = Samu_bodies[1].transform.position;
-            eye_obj.transform.position = Samu_bodies[1].transform.position;
-            if(Samu_bodies[1].transform.position.x <= cam_bounds_.x + init_pos.x/2)
-            {
-                Debug.Log("AAA");
-            }
+            MoveOnScreen(dash_number);
         }
-
+        
+       
     }
 
 
-    private void TransferBody()
+    private void Move_track_dash(int direction)
     {
-       
+
+        float dir;
+        float max_dir;
+        if (direction != 2)
+        {
+            //RIGHT 2 LEFT
+            if (direction == 0)
+            {
+                dir = cam_bounds_.x + init_pos.x / 2 - 15;
+                max_dir = cam_bounds_.x - init_pos.x / 2 - 100;
+
+                if (!tracking)
+                {
+                    if (currentBody.transform.position.x <= dir)
+                    {
+                        tracking = true;
+
+                    }
+                    else
+                    {
+                        Vector3 EndPos = new Vector3(dir, player.transform.position.y + 10, init_wPos.z - init_pos.z);
+
+                        float damp_f = 1f;
+
+                        MoveToPoint(currentBody.transform, EndPos, damp_f, 1);
+                    }
+                }
+            }
+            // LEFT 2 RIGHT
+            else
+            {
+                dir = cam_bounds_.x - init_pos.x / 2 + 15;
+                max_dir = cam_bounds_.x + init_pos.x / 2 + 100;
+
+                if (!tracking)
+                {
+                    if (currentBody.transform.position.x >= dir)
+                    {
+                        tracking = true;
+
+                    }
+                    else
+                    {
+                        Vector3 EndPos = new Vector3(dir, player.transform.position.y + 10, init_wPos.z - init_pos.z);
+
+                        float damp_f = 1f;
+
+                        MoveToPoint(currentBody.transform, EndPos, damp_f, 1);
+                    }
+                }
+            }
+
+
+
+
+
+
+            if (tracking)
+            {
+                if (trackingTimer_time < trackingTimer)
+                {
+                    MoveToPoint(currentBody.transform, new Vector3(dir, player.transform.position.y + 10, init_wPos.z - init_pos.z), 1.5f, 1);
+                    currentBody.transform.position = new Vector3(dir, currentBody.transform.position.y, currentBody.transform.position.z);
+                    trackingTimer_time += Time.deltaTime;
+
+                }
+                else
+                {
+                    Vector3 EndPos = new Vector3(max_dir, currentBody.transform.position.y, currentBody.transform.position.z);
+                    float damp_f = 1;
+
+
+                    MoveToPoint(currentBody.transform, new Vector3(max_dir, currentBody.transform.position.y, currentBody.transform.position.z), dash_velocity * damp_f, 1);
+                }
+                if (currentBody.transform.position.x == max_dir)
+                {
+                    trackingTimer_time = 0;
+                    dash_number++;
+                    tracking = false;
+
+                }
+            }
+        }
+        else
+        {
+
+
+        }
+
+
+
+
+
+    }
+    private void MoveOnScreen(int dash_number) {
+
+        MainBodyParts[(int)(BodyParts.INNER_RING)].SetActive(false);
+        MainBodyParts[(int)(BodyParts.OUTER_RING)].SetActive(false);
+        MainBodyParts[(int)(BodyParts.CORE)].SetActive(false);
+      
+        float prev_x = currentBody.transform.position.x;
+
+
+        switch (dash_number)
+        {
+            case 1:
+
+                Move_track_dash(0);
+
+                break;
+               
+            case 2:
+                Move_track_dash(1);
+
+                break;
+                
+            case 3:
+                if (enraged) { Move_track_dash(2); }
+                else
+                {
+                    Move_track_dash(0);
+                }
+                break;
+            case 4:
+                break;
+
+        }
+
+        if (dash_number > max_dash_number) { Atk2Finished = true; }
+        float cur_x = currentBody.transform.position.x;
+
+
+    }
+
+     private void TransferBody(GameObject Body )
+    {
         Vector3 cam_bounds_ = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
         Vector3 init_wPos = transform.TransformPoint(init_pos);
-        Samu_bodies[1].transform.position = new Vector3(cam_bounds_.x + init_pos.x / 2 + 100, player.transform.position.y, init_wPos.z-init_pos.z) ;
-        mainCircle.transform.position = Samu_bodies[1].transform.position;
-        eye_obj.transform.position = Samu_bodies[1].transform.position;
-        Samu_bodies[1].SetActive(true);
+        if (Body == Samu_bodies[1])
+        {
+            Body.transform.position = new Vector3(cam_bounds_.x + init_pos.x / 2 + 100, player.transform.position.y+10, init_wPos.z - init_pos.z);
+        }
+        mainCircle.transform.position = Body.transform.position;
+        eye_obj.transform.position = Body.transform.position;
+        currentBody.SetActive(false);
+        Body.SetActiveRecursively(true);
+        currentBody = Body;
+
     }
 
     public Samu_BigFireball[] ATK1 () {
@@ -369,9 +579,11 @@ public class Samu_animation_script : MonoBehaviour
 
         prev_state = state;
         next_state = Anim_States.ATK2;
+        max_dash_number = 2;
+        if (enraged) { max_dash_number = 3; }
         changeState = true;
         cameraOffscreenPoint =  Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        point = new Vector3(transform.TransformPoint(init_pos).x-init_pos.x , cameraOffscreenPoint.y - init_pos.y, 500);
+        point = new Vector3(transform.TransformPoint(init_pos).x , cameraOffscreenPoint.y - init_pos.y, 500);
 
     }
     public void Stop () {
@@ -511,6 +723,10 @@ public class Samu_animation_script : MonoBehaviour
     public bool playerInRange
     {
         set { playerOnSight = value; }
+    }
+    public float dash_Speed
+    {
+        set { dash_velocity = value; }
     }
 
     public GameObject player_ref
